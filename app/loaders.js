a4bb67755f2edf7e6e07c5b33f768ef5e2a738d9
altsab <altsab@yandex.com>
2017-11-22 18:02:06 +0600

    add proper dataloader batching

--- app/loaders.js
+++ app/loaders.js
@@ -1,16 +1,13 @@
 const DataLoader = require('dataloader');
 const { executeQuery } = require('./db');
+const { keyBy } = require('lodash');
 
 const hostsLoader = new DataLoader(async (keys) => {
   const keysList = keys.join(', ');
   try {
     const data = await executeQuery(`SELECT * FROM hosts WHERE id IN (${keysList}) ORDER BY array_position(ARRAY[${keysList}], id)`);
-    const resultLength = data.rows.length;
-    const queryLength = keys.length;
-    if (resultLength !== queryLength) {
-      throw new Error('Result length is not valid');
-    }
-    return data.rows;
+    const hostsById = keyBy(data.rows, 'id');
+    return keys.map(id => hostsById[id]);
   } catch (err) {
     console.log(err);
     return null;
@@ -21,12 +18,8 @@ const resultsLoader = new DataLoader(async (keys) => {
   const keysList = keys.join(', ');
   try {
     const data = await executeQuery(`SELECT * FROM results WHERE id IN (${keysList}) ORDER BY array_position(ARRAY[${keysList}], id)`);
-    const resultLength = data.rows.length;
-    const queryLength = keys.length;
-    if (resultLength !== queryLength) {
-      throw new Error('Result length is not valid');
-    }
-    return data.rows;
+    const resultsById = keyBy(data.rows, 'id');
+    return keys.map(id => resultsById[id]);
   } catch (err) {
     console.log(err);
     return null;
